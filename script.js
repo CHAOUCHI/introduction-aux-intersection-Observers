dab4f07646e45d32674cb4fa113b7e6f21f16fe5
CHAOUCHI <massi.chaouchi@gmail.com>
2023-02-05 03:55:32 +0100

    Mise à jour de l'affichage pour une meilleur compréhension.
    + ajout d'une fenêtre modale pour affiche le pourcentage de vue des sections.
    + Je regarde les sections impaire uniquement.
    + Ajout de la fonction getArray100() qui renvoi un table qui contient une suite tel que Un+1 = Un+0.1 où U0 = 0
    Cela me permet d'exécuter la fonction callback de l'observer à chaque dixième de pourcent.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /**
  * 1. Je récupére la section 3
  */
-const section2 = document.querySelectorAll(".container")[1];
-const section4 = document.querySelectorAll(".container")[3];
+const section1 = document.querySelectorAll(".container")[0];
+const section3 = document.querySelectorAll(".container")[2];
+const section5 = document.querySelectorAll(".container")[4];
 
 /**
  * 2. Je créer l'observer qui va excuter une function callback quand
@@ -16,17 +17,26 @@ let observer = new IntersectionObserver(function(entries,observer){
     /**
      * 3. Je vérifie si l'element est bien vu, cela évite d'executer la callback 
      * quand l'element sort de notre champs de vision
-     * */ 
+     * */
     if(entries[0].isIntersecting === true){
-        const pourcentage_element_a_lecran = (entries[0].intersectionRatio*100).toFixed(2); 
+        const pourcentage_element_a_lecran = Math.round((entries[0].intersectionRatio*100)); 
         const sectionName = entries[0].target.lastElementChild.innerHTML;
+        const section_titre = document.querySelector(".modal>.container__titre");
+        section_titre.innerText = `Je vois la ${sectionName} à ${pourcentage_element_a_lecran}% `;
         console.log(`Je te vois à ${pourcentage_element_a_lecran}% !`,sectionName)   
     }
 },{
-    threshold:0.5 //La fonction callback est executée si l'element est vu à 50%.
+    threshold:getArray100()//La fonction callback est executée a chaque 1% de decouverte du array
 });
 
 
 // 4. J'observe la section 3
-observer.observe(section2);
-observer.observe(section4);
+observer.observe(section1);
+observer.observe(section3);
+observer.observe(section5);
+//Je ne regarde pas la section 5
+
+
+function getArray100(){
+    return [...Array(100).keys()].map(it=>it/100) 
+}
